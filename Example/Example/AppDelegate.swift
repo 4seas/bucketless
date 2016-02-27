d4c7d0b05d25725a0fc0214baa4c33b72435995b
Chad Lou <chadlou@yahoo.com>
2016-02-27 15:03:36 -0800

    json parsing for cities

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -21,7 +21,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         application.registerForRemoteNotifications()
         
         UIApplication.sharedApplication().statusBarHidden = true
+        test()
         return true
+    }
+    
+    func test() {
+        if let path = NSBundle.mainBundle().pathForResource("cities", ofType: "json") {
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let jsonObj = JSON(data: data)
+                if jsonObj != JSON.null {
+                    let dict = jsonObj.dictionary!
+                    for key in dict.keys {
+                        let desc = dict[key]!["description"]
+                        print("key: \(key) description \(desc)")
+                    }
+                    print("jsonData:\(jsonObj)")
+                } else {
+                    print("could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
     }
 
     func applicationWillResignActive(application: UIApplication) {
