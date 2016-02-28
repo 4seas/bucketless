bb9762e65d0997b93b64496a8bdade3201a48a40
Chad Lou <chadlou@yahoo.com>
2016-02-27 16:11:34 -0800

    add button handler for UIImage

--- Example/Example/DetailVC.swift
+++ Example/Example/DetailVC.swift
@@ -13,14 +13,35 @@ class DetailVC: UIViewController {
     @IBOutlet weak var locationdescrip: UITextView!
     @IBOutlet weak var imageitem: UIImageView!
     @IBOutlet weak var locationname: UILabel!
+    @IBOutlet weak var addImage: UIImageView!
+    @IBOutlet weak var checkImage: UIImageView!
     var imagename: String!
     var locationtext: String!
     override func viewDidLoad() {
         super.viewDidLoad()
         locationname.text = locationtext
         imageitem!.image = UIImage(named: imagename)
+
+        var imageView = checkImage
+        var tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("checkImageTapped:"))
+        imageView.userInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+
+        imageView = addImage
+        tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("addImageTapped:"))
+        imageView.userInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
     }
 
+    func checkImageTapped(img: AnyObject) {
+        locationdescrip.text = "check image tapped"
+        print("check image tappped")
+    }
+    
+    func addImageTapped(img: AnyObject) {
+        print("add image tapped")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
