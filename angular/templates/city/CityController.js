82e03dbae1e16b7201b8aad2d2649f6abab736ea
Chad Lou <chadlou@yahoo.com>
2016-02-29 20:47:48 -0800

    show the details about the chosen city and an offer

--- angular/templates/city/CityController.js
+++ angular/templates/city/CityController.js
@@ -1,5 +1,5 @@
 app.controller('cityCtrl', function ($scope, $location, $routeParams) {
 	$scope.header = true;
-  // $scope.cities = cities;
   $scope.city = cities[$routeParams.id]
+  $scope.title = $scope.city.city;
 });
