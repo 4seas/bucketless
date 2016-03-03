5a61372fc6d44a5be35c1e1204d4d875d61f266a
Chad Lou <chadlou@yahoo.com>
2016-03-02 22:20:05 -0800

    use local images for ionic

--- angular/js/dataServices.js
+++ angular/js/dataServices.js
@@ -115,7 +115,7 @@ var places = [
 	{
 		id: 3,
 		name: 'Paris',
-		img: 'images/places4.jpg',
+		img: 'images/places5.jpg',
 		people: [
       {
 				img: 'images/people/gissele.png',
@@ -166,40 +166,41 @@ var cities =
 		id: 1,
 		"city": "London",
 	  "description" : "London is the capital city of England and the United Kingdom. London has been a major settlement for centuries and affords its visitors great tourist attractions.",
-	  "image": "https://www.imperial.ac.uk/ImageCropToolT4/imageTool/uploaded-images/LONDON_shutterstock_229478404--tojpeg_1417791048879_x1.jpg",
+	  "image": "images/cities/london.jpg",
+      //"https://www.imperial.ac.uk/ImageCropToolT4/imageTool/uploaded-images/LONDON_shutterstock_229478404--tojpeg_1417791048879_x1.jpg",
 	  "title_offer": "Go see the London Eye for half off!",
 	  "offer_cost": "$15",
-	  "image_offer": "http://cdn.londonandpartners.com/asset/20adda9d08e8480c6dbbfcf30fbcabdb.jpg"
+	  "image_offer": "images/cities/London_deal.jpg" //"http://cdn.londonandpartners.com/asset/20adda9d08e8480c6dbbfcf30fbcabdb.jpg"
   },
 
   {
 		id: 2,
 		"city": "Paris",
     "description": "Paris has always been one of the major tourist destinations in Europe. The city is situated on the banks of the Seine River and is one of the largest European capitals. The metropolitan area of the city is one of the most populated in Europe, about 11.5 million people live in it",
-    "image": "https://www.eanamed.eu/images/Places/paris-6.jpg",
+    "image": "images/cities/paris.jpg", //"https://www.eanamed.eu/images/Places/paris-6.jpg",
     "title_offer": "See the Louvre Museam museum for 30% off through thursday!",
     "offer_cost": "$12",
-    "image_offer": "http://fasttravel.info/wp-content/uploads/2016/01/louvre-museum.jpg"
+    "image_offer": "images/cities/Paris_deal.jpg" //"http://fasttravel.info/wp-content/uploads/2016/01/louvre-museum.jpg"
   },
 
 	{
 		id: 3,
 		  "city": "Mexico City",
       "description": "the capital of México, very old, the worlds largest city, the financial, political and cultural center of México, the nightlife capital of México, one of the worlds great cities, huge, one of the world's most difficult cities to drive in, filled with exciting things to see and do, sinking, an energetic metropolis or just an incredible place to visit.",
-      "image": "http://cdn1.theodysseyonline.com/files/2015/12/20/635862387549042491-673939441_Overview-of-Mexico-City-001.jpg",
+      "image": "images/cities/MexicoCity.jpg", //"http://cdn1.theodysseyonline.com/files/2015/12/20/635862387549042491-673939441_Overview-of-Mexico-City-001.jpg",
       "title_offer": "This week only, $10 off admission to the Palacio de Bellas Artes",
       "offer_cost": "$5",
-      "image_offer": "http://periodiconmx.com/wp-content/uploads/2015/06/bellas_artes_by_laloxxx-d3af398.jpg"
+      "image_offer": "images/cities/MexicoCity_deal.jpg" //"http://periodiconmx.com/wp-content/uploads/2015/06/bellas_artes_by_laloxxx-d3af398.jpg"
   },
 
 	{
 		id: 4,
 		"city": "Tokyo",
     "description": "Tokyo is the capital of Japan and is located in the Kanto region of Central Honsu, next to Tokyo Bay. The city is located on a floodplain of three large rivers: the Sumida River, the Ara River and the Edo River. The Sumida River flows through the centre of the Metropolis and is integrated in the urban pattern of the city. The Edo River and the Ara River are located in the outer districts of Tokyo. Large parts of Tokyo are located below the flood level of its main rivers." ,
-    "image":"https://ict.swisscom.ch/wp-content/uploads/2015/09/tokyo.jpg",
+    "image":"images/cities/Tokyo.jpg", //"https://ict.swisscom.ch/wp-content/uploads/2015/09/tokyo.jpg",
     "title_offer": "Get $20 in free pachinko credits when you spend $20 at pachinko central",
     "offer_cost": "$20",
-    "image_offer": "https://upload.wikimedia.org/wikipedia/commons/2/2a/Electric_City_Akihabara_Pachinko.jpg"
+    "image_offer": "images/cities/Tokyo_deal.jpg" //"https://upload.wikimedia.org/wikipedia/commons/2/2a/Electric_City_Akihabara_Pachinko.jpg"
   },
 /*
 	{
@@ -251,10 +252,10 @@ var cities =
 			 id: 4,
 	 		  "city": "New York",
         "description" : "New York City is the Capital city of New York State and is the most populated city in the United States. It is most of the most visited cities in the world. Some of most popular tourist attractions are Times Square, Rockefeller Center, Central Park, and the Guggenheim Museaum.",
-        "image": "http://www.ssn.tv/wp-content/uploads/2014/07/NYC-1.jpg",
+        "image": "images/cities/NYC.jpg", //"http://www.ssn.tv/wp-content/uploads/2014/07/NYC-1.jpg",
         "title_offer": "Buy one get one free admission to the Guggenheim Museaum.",
         "offer_cost": "$29.99",
-        "image_offer": "http://res.cloudinary.com/dk/image/upload/q_80,c_fit,w_1200,h_630/new-york/solomon-guggenheim-museum.jpg"
+        "image_offer": "images/cities/NYC_deal.jpg" //"http://res.cloudinary.com/dk/image/upload/q_80,c_fit,w_1200,h_630/new-york/solomon-guggenheim-museum.jpg"
      }
 
 ];
