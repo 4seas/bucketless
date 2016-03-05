36d63c5a9d38887b6f70cf57464020e16e9bdf3c
Chad Lou <chadlou@yahoo.com>
2016-03-05 15:32:02 -0800

    change to blue background when clicked

--- angular/js/controller.js
+++ angular/js/controller.js
@@ -10,6 +10,24 @@ app.controller('indexController', function ($scope, $location, $routeParams) {
 	// 	console.log($scope.header);
 	// 	return $scope.header;
 	// }
+
+	$scope.iconClass1 = $scope.iconClass2 = $scope.iconClass3 = "nav__icon";
+
+	$scope.changeClass = function(iconIndex) {
+		console.log("changeClass for " + iconIndex);
+		if(iconIndex == 1) {
+			$scope.iconClass1 = "nav__icon icon--circle";
+			$scope.iconClass2 = $scope.iconClass3 = "nav__icon";
+		}
+		if(iconIndex == 2) {
+			$scope.iconClass2 = "nav__icon icon--circle";
+			$scope.iconClass1 = $scope.iconClass3 = "nav__icon";
+		}
+		if(iconIndex == 3) {
+			$scope.iconClass3 = "nav__icon icon--circle";
+			$scope.iconClass1 = $scope.iconClass2 = "nav__icon";
+		}
+	}
 });
 
 app.controller('signInCtrl', function ($scope, $location, $routeParams) {
