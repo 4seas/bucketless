f32f07e161744a3071f9be8c1bbc0965fbad790b
Chad Lou <chadlou@yahoo.com>
2016-03-07 20:39:41 -0800

    use inception date

--- angular/templates/profile/ProfileController.js
+++ angular/templates/profile/ProfileController.js
@@ -1,7 +1,7 @@
 app.controller('profileCtrl', function ($scope, $location, $routeParams) {
 	$scope.header = true;
 	$scope.title = 'My Profile';
-	$scope.date = Date.now();
+	$scope.date = 'February 27, 2016'; //Date.now();
 	$scope.user = user[0];
 	$scope.places = places;
 
